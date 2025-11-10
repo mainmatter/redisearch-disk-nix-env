3579a84e4e58d2b745357bfe2a8cbe6d426a9b49
chesedo <pieter@chesedo.me>
2025-11-10 14:36:50 +0200

    Initial env

---
+++ flake.nix
@@ -0,0 +1,86 @@
+{
+  description = "Build a RediSearch on Disk Rust library";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    redis-flake = {
+      url = "github:chesedo/redis-flake/zlib";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.redis.url = "git+ssh://git@github.com/redislabsdev/Redis.git";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, redis-flake, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+          config.allowUnfree = true;
+        };
+        redis-source = redis-flake.packages.${system}.redis;
+      in
+      {
+        devShells = {
+          default =  pkgs.mkShell {
+            hardeningDisable = [ "fortify" "stackprotector" "pic" "relro" ];
+            # Shell hooks to create executable scripts in a local bin directory
+            shellHook = ''
+              cargo_version=$(cargo --version 2>/dev/null)
+
+              echo -e "\033[1;36m=== 🦀 Welcome to the RediSearch development environment ===\033[0m"
+              echo -e "\033[1;33m• $cargo_version\033[0m"
+              echo -e "\n\033[1;33m• Checking for any outdated packages...\033[0m\n"
+              cd redisearch_disk && cargo outdated --root-deps-only
+
+              # For libclang dependency to work
+              export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
+              # For `sys/types.h` and `stddef.h` required by redismodules-rs
+              export BINDGEN_EXTRA_CLANG_ARGS="-I${pkgs.glibc.dev}/include -I${pkgs.gcc-unwrapped}/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/include"
+
+              # Force SVS to build from source instead of using precompiled library
+              export CMAKE_ARGS="-DSVS_SHARED_LIB=OFF"
+
+              # Needed to build speedb from source
+              export NIX_CFLAGS_COMPILE="-Wno-error=format-truncation $NIX_CFLAGS_COMPILE"
+            '';
+
+            buildInputs = with pkgs; [
+              # For LSP
+              ccls
+
+              # Dev dependencies based on developer.md
+              cmake
+              openssl.dev
+              libxcrypt
+
+              zlib
+
+              rust-bin.stable.latest.default
+
+              # For search on disk
+              redis-source
+              boost188
+              liburing
+            ];
+
+            packages = with pkgs; [
+              rust-analyzer
+              cargo-watch
+              cargo-outdated
+              lldb
+              vscode-extensions.vadimcn.vscode-lldb
+            ];
+          };
+        };
+      });
+}
